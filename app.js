cb5edee66eb0f3b6982ee2d0ac60252a6a487390
Yu Yang <yu.yang@cittacorp.com>
2017-11-02 02:00:52 -0400

    jslint change

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-var express 			= require('express'),
-		logger 				= require('morgan'),
-		mongoose 			= require('mongoose'),
-		fs 						= require('fs');
+var express = require('express'),
+  logger = require('morgan'),
+  mongoose = require('mongoose'),
+  fs = require('fs')
 
 // all environments
-var app = express();
-app.use(logger('dev'));
-app.use(express.static('public'));
-app.use(express.static(`${__dirname}/public`));
+var app = express()
+app.use(logger('dev'))
+app.use(express.static('public'))
+app.use(express.static(`${__dirname}/public`))
 
-//Connect to mongodb in the cloud server or local server database
-var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost/energy-data';
+// Connect to mongodb in the cloud server or local server database
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost/energy-data'
 mongoose.connect(mongoUrl, function (err) {
-  if(err){
-    console.log(err);
-  }else {
-    console.log('connection successful');
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('connection successful')
   }
-});
+})
 
-//Set up the port to listen
-var port = (parseFloat(process.env.PORT)) || 3000;
+// Set up the port to listen
+var port = (parseFloat(process.env.PORT)) || 3000
 app.listen(port, function () {
-  console.log('App listening on port '+port+'...');
-});
+  console.log('App listening on port ' + port + '...')
+})
 
-//use fs to load controller file
-fs.readdirSync('./controllers').forEach(function (file) {
-  if(file.substr(-3) == '.js') {
-      route = require('./controllers/' + file);
-      route.controller(app);
-  }
-});
+// use fs to load controller file
+fs
+  .readdirSync('./controllers')
+  .forEach(function (file) {
+    if (file.substr(-3) === '.js') {
+      var route = require('./controllers/' + file)
+      route.controller(app)
+    }
+  })
